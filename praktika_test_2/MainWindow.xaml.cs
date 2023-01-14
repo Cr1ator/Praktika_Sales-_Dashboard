5254a93aee68d3040c7bdbda66dc70f571683761
ddoshechko <ddoshechko@bs-solutions.by>
2023-01-14 18:27:10 +0300

    main menu

--- praktika_test_2/MainWindow.xaml.cs
+++ praktika_test_2/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace praktika_test_2
 {
@@ -23,6 +11,37 @@ namespace praktika_test_2
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton==MouseButton.Left) 
+            { 
+                this.DragMove();
+            }
+        }
+
+        bool IsMaximized= false;
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (this.IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Width = 1280;
+                    this.Height = 780;
+
+                    this.IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState= WindowState.Maximized;
+
+                    this.IsMaximized = true;
+                }
+            }
         }
     }
 }

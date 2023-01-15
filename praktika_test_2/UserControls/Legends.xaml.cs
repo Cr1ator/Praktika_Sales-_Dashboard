f7b001deb7b9bb8ba533b525b8daa8a263191cdf
ddoshechko <ddoshechko@bs-solutions.by>
2023-01-15 17:03:42 +0300

    main chart

---
+++ praktika_test_2/UserControls/Legends.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace praktika_test_2.UserControls
+{
+    public partial class Legends : UserControl
+    {
+        public Legends()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
